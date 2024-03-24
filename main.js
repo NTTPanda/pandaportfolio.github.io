8cf4871c8d14bb7754932668ff5e28721972f2c0
NTTPanda <rohitkrishnagouda@gmail.com>
2024-03-24 21:31:10 +0530

    added information about education

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ var typed = new Typed('.text', {
     backDealy:1000,
     loop:true
 });
+
+
+// Smooth scrolling for anchor links
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         const targetId = this.getAttribute('href').substring(1);
+//         const target = document.getElementById(targetId);
+
+//         if (target) {
+//             window.scrollTo({
+//                 top: target.offsetTop,
+//                 behavior: 'smooth'
+//             });
+//         }
+//     });
+// });
